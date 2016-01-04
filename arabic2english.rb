10bb21072f8ea8dee7dbb1dbea96619f542b97f0
Harvey Ico <godstrikerharvey@gmail.com>
2016-01-04 19:49:59 +0800

    Implements number from 20-99

--- arabic2english.rb
+++ arabic2english.rb
@@ -1,33 +1,33 @@
 class Arabic2English
   def base_numbers
     {
-      1 => 'one',
-      2 => 'two',
-      3 => 'three',
-      4 => 'four',
-      5 => 'five',
-      6 => 'six',
-      7 => 'seven',
-      8 => 'eight',
-      9 => 'nine',
-      10 => 'ten',
-      11 => 'eleven',
-      12 => 'twelve',
-      13 => 'thirteen',
-      14 => 'fourteen',
-      15 => 'fifteen',
-      16 => 'sixteen',
-      17 => 'seventeen',
-      18 => 'eighteen',
-      19 => 'nineteen',
-      20 => 'twenty',
-      30 => 'thiry',
-      40 => 'fourty',
-      50 => 'fifty',
-      60 => 'sixty',
-      70 => 'seventy',
+      90 => 'ninety',
       80 => 'eighty',
-      90 => 'ninety'
+      70 => 'seventy',
+      60 => 'sixty',
+      50 => 'fifty',
+      40 => 'fourty',
+      30 => 'thiry',
+      20 => 'twenty',
+      19 => 'nineteen',
+      18 => 'eighteen',
+      17 => 'seventeen',
+      16 => 'sixteen',
+      15 => 'fifteen',
+      14 => 'fourteen',
+      13 => 'thirteen',
+      12 => 'twelve',
+      11 => 'eleven',
+      10 => 'ten',
+      9 => 'nine',
+      8 => 'eight',
+      7 => 'seven',
+      6 => 'six',
+      5 => 'five',
+      4 => 'four',
+      3 => 'three',
+      2 => 'two',
+      1 => 'one'
     }
   end
 
@@ -38,11 +38,12 @@ class Arabic2English
 
     base_numbers.each do |num, text|
       if integer.to_s.length == 1 && integer/num > 0
-        result += "#{text}"
+        return result + text
+      elsif integer.to_s.length < 100 && integer/num > 0
+        return result + text if integer%num == 0
+        return result + text + ' ' + translate!(integer%num)
       end
     end
-
-    result
   end
 end
 
--- arabic2english_spec.rb
+++ arabic2english_spec.rb
@@ -5,17 +5,19 @@ describe Arabic2English do
   let!(:arabic_to_english) { Arabic2English.new }
 
   describe '#translate!' do
-    it 'translates common arabic number into english' do
-      expect(arabic_to_english.translate!(1)).to eq 'one'
+    it 'translates arabic number of 1-10 into english' do
+      expect(arabic_to_english.translate!(9)).to eq 'nine'
     end
 
-    it 'translate arabic number of tenths into english' do
+    it 'translate arabic number of greater than 10' do
       expect(arabic_to_english.translate!(11)).to eq 'eleven'
     end
 
-    it 'translate arabic number of tenths above into english' do
-      expect(arabic_to_english.translate!(41)).to eq 'twenty one'
+    it 'translate arabic number of greather than 19 and less than 100' do
+      expect(arabic_to_english.translate!(51)).to eq 'fifty one'
     end
+
+
   end
 
 end
