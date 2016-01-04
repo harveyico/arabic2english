9dea0e9e910b8c6165364ef10e47771f67c1330e
Harvey Ico <godstrikerharvey@gmail.com>
2016-01-04 17:59:56 +0800

    Adds simple translator from 1 to 5

--- arabic2english.rb
+++ arabic2english.rb
@@ -1,7 +1,20 @@
 class Arabic2English
-  def self.translate!(num)
-    puts num
+  def temp_mapping
+    {
+      '1' => 'one',
+      '2' => 'two',
+      '3' => 'three',
+      '4' => 'four',
+      '5' => 'five',
+    }
+  end
+
+  def translate!(num)
+    temp_mapping[num.to_s]
   end
 end
 
-puts ARGV
+arabic_to_english = Arabic2English.new
+ARGV.each do |num|
+  puts arabic_to_english.translate!(num)
+end
