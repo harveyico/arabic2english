602536e5cf5199727cb60f429a2874d6348b9ad8
Harvey Ico <godstrikerharvey@gmail.com>
2016-01-04 19:53:15 +0800

    Adds spec for 100-999

--- arabic2english_spec.rb
+++ arabic2english_spec.rb
@@ -9,14 +9,17 @@ describe Arabic2English do
       expect(arabic_to_english.translate!(9)).to eq 'nine'
     end
 
-    it 'translate arabic number of greater than 10' do
+    it 'translate arabic number of 11-19 into english' do
       expect(arabic_to_english.translate!(11)).to eq 'eleven'
     end
 
-    it 'translate arabic number of greather than 19 and less than 100' do
+    it 'translate arabic number of 20-99 into english' do
       expect(arabic_to_english.translate!(51)).to eq 'fifty one'
     end
 
+    it 'translate arabic number of 100-999 into english' do
+      expect(arabic_to_english.translate!(171)).to eq 'one hundred seventy one'
+    end
 
   end
 
